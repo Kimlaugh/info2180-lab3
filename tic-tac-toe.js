c2b08d604096162b8a368cb5146e8cbf5127d046
Kimberly Laughton <kimberlyclaughton@gmail.com>
2022-10-14 18:58:57 -0500

    Added click function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,3 +6,39 @@ function layout()
         division.className = "square";
     }
 }
+
+function playGame(){
+    var turn = 1;
+    var board = document.getElementById("board").getElementsByTagName("div");
+    for (const division of board)
+    {
+        division.addEventListener("mouseover", function(){
+            if(division.textContent == ""){
+                division.classList.add("hover");
+            }
+        })
+
+        division.addEventListener("mouseout", function(){
+            division.classList.remove("hover");
+
+        })
+
+        division.addEventListener("click", function(){
+            if (turn == 1 && division.textContent != "O"){
+                division.classList.add("X");
+                division.textContent ="X";
+                turn = 0;
+                whoIsWinner();
+            }
+            else{
+                if (turn == 0 && division.textContent != "X"){
+                    division.classList.add("O");
+                    division.textContent = "O";
+                    turn = 1;
+                    whoIsWinner();
+                }
+            }
+
+        })
+    }
+}
